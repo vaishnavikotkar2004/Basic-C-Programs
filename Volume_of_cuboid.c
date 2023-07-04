109c86141ebca88a03c964bd239754c0df52bcc9
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-07-04 20:36:56 +0530

    Add files via upload

---
+++ Volume_of_cuboid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float w,h,l,v;
+ printf("enter width");
+ scanf("%f",&w);
+ printf("enter height");
+ scanf("%f",&h);
+ printf("enter length");
+ scanf("%f",&l);
+ v=w*h*l;
+ printf("\n volume=%f",v);
+}
