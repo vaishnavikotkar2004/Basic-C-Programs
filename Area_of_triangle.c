8a4ffb9ff6d65d6f10948cc461b232d37a9f6fd5
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-07-04 19:56:04 +0530

    Add files via upload

---
+++ Area_of_triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ float b,h,a;
+ printf("enter breadth");
+ scanf("%f",&b);
+ printf("enter height");
+ scanf("%f",&h);
+ a=0.5*b*h;
+ printf("\n area of triangle=%f",a);
+}
