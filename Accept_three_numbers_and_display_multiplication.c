e111ed54ffc1d94d107f704ea6ab094a110c7461
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-07-04 19:58:21 +0530

    Add files via upload

---
+++ Accept_three_numbers_and_display_multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c,m;
+ printf("enter first number");
+ scanf("%d",&a);
+ printf("enter second number");
+ scanf("%d",&b);
+ printf("enter third number");
+ scanf("%d",&c);
+ m=a*b*c;
+ printf("\n multiplication=%d",m);
+}
